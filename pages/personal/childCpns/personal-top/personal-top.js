3f9d807705db4a1d4c0927ba1f068c4000328d4b
DESKTOP-KRK8038\Chen <18382310570@163.com>
2020-04-01 17:15:52 +0800

    新增用户中心

---
+++ pages/personal/childCpns/personal-top/personal-top.js
@@ -0,0 +1,22 @@
+// pages/personal/childCpns/personal-top/personal-top.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -5,7 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userAvatar:'/assets/personal/avatar.png',
+    userName:'登录',
+    isLogin:true,
+    item1:[
+      {icon:'message.png',msg:'我的消息'},
+      {icon:'pointer.png',msg:'积分商城'},
+      {icon:'vip.png',msg:'会员卡'},
+    ],
+    item2:[
+      {icon:'cart.png',msg:'我的购物车'},
+      {icon:'shopping.png',msg:'下载购物APP'},
+    ]
   },
 
   /**
@@ -14,7 +25,23 @@ Page({
   onLoad: function (options) {
 
   },
-
+  handleGetUserInfo(res){
+    console.log(res);
+    const userInfo = res.detail.userInfo;
+    if(userInfo){
+    
+      this.setData({
+        userAvatar: userInfo.avatarUrl,
+        userName: userInfo.nickName,
+        isLogin: false
+      })
+      try {
+        wx.setStorageSync('userInfo', userInfo)
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -26,6 +53,16 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    const userInfo = wx.getStorageSync('userInfo')
+    if (userInfo){
+      this.setData({
+        userAvatar: userInfo.avatarUrl,
+        userName: userInfo.nickName,
+        isLogin:false
+      })
+    }
+
+
 
   },
 
